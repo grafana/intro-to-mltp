2ff01e344f9f15aa908a46008936f091ed7ab838
Heds Simons <heds@whaleway.net>
2022-06-08 22:01:32 -0700

    Adds resource attr detection, latest Tempo, code changes.
    Code changes are required because of resource attr
    detection, which hangs off a promise. As such,
    code needs to now become async and require libraries
    after the trace intialisation has occured (for
    auto-instrumented server code).
    
    Signed-off-by: Heds Simons <heds@whaleway.net>

--- source/common/logging.js
+++ source/common/logging.js
@@ -1,60 +1,61 @@
-const tracing = require('./tracing')();
 const request = require('request-promise-native');
 
 module.exports = (serviceName, context) => {
-    // Tracing
-    const { api, tracer } = tracing;
+    return async (tracingObj) => {
+        // Tracing
+        const { api, tracer } = tracingObj;
 
-    // Logging system sends to Loki
-    const logEntry = async (details) => {
-        const { message, level, job, endpointLabel, endpoint, namespace } = details;
-        let logSpan;
-        if (context === 'requester') {
-            // Create a new span
-            logSpan = tracer.startSpan("log_to_loki", { kind: api.SpanKind.CLIENT });
-        }
-        let error = false;
-        let stream = {
-            service_name: serviceName,
-            level,
-            job,
-            namespace,
-        };
-        if (endpoint) {
-            stream[endpointLabel] = endpoint;
-        }
+        // Logging system sends to Loki
+        const logEntry = async (details) => {
+            const { message, level, job, endpointLabel, endpoint, namespace } = details;
+            let logSpan;
+            if (context === 'requester') {
+                // Create a new span
+                logSpan = tracer.startSpan("log_to_loki", { kind: api.SpanKind.CLIENT });
+            }
+            let error = false;
+            let stream = {
+                service_name: serviceName,
+                level,
+                job,
+                namespace,
+            };
+            if (endpoint) {
+                stream[endpointLabel] = endpoint;
+            }
 
-        try {
-            await request(
-                {
-                    uri: process.env.LOGS_TARGET,
-                    method: 'POST',
-                    headers: {
-                        'Content-type': 'application/json'
+            try {
+                await request(
+                    {
+                        uri: process.env.LOGS_TARGET,
+                        method: 'POST',
+                        headers: {
+                            'Content-type': 'application/json'
+                        },
+                        json: true,
+                        body: {
+                            'streams': [
+                                {
+                                    stream,
+                                    'values': [
+                                        [ `${Date.now() * 1000000}`, message ]
+                                    ]
+                                }
+                            ]
+                        }
                     },
-                    json: true,
-                    body: {
-                        'streams': [
-                            {
-                                stream,
-                                'values': [
-                                    [ `${Date.now() * 1000000}`, message ]
-                                ]
-                            }
-                        ]
-                    }
-                },
-            );
-        } catch (err) {
-            console.log(`Logging error: ${err}`);
-            error = true;
-        }
-        if (context === 'requester') {
-            // Set the status code as OK and end the span
-            logSpan.setStatus({ code: (!error) ? api.SpanStatusCode.OK : api.SpanStatusCode.ERROR });
-            logSpan.end();
-        }
-    };
+                );
+            } catch (err) {
+                console.log(`Logging error: ${err}`);
+                error = true;
+            }
+            if (context === 'requester') {
+                // Set the status code as OK and end the span
+                logSpan.setStatus({ code: (!error) ? api.SpanStatusCode.OK : api.SpanStatusCode.ERROR });
+                logSpan.end();
+            }
+        };
 
-    return logEntry;
+        return logEntry;
+    };
 }
--- source/common/tracing.js
+++ source/common/tracing.js
@@ -9,77 +9,83 @@ module.exports = (context, serviceName) => {
   const { SimpleSpanProcessor } = require("@opentelemetry/sdk-trace-base");
   const { JaegerExporter } = require('@opentelemetry/exporter-jaeger');
   const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-grpc');
-  const { Resource } = require('@opentelemetry/resources');
+  const { envDetector, Resource } = require('@opentelemetry/resources');
   const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');
-  let getNodeAutoInstrumentations;
-  let registerInstrumentations;
-  let W3CTraceContextPropagator;
 
-  // If this is the server, it's all autoinstrumented.
-  // Else include the propagator.
-  if (context === 'server') {
-    getNodeAutoInstrumentations = require('@opentelemetry/auto-instrumentations-node').getNodeAutoInstrumentations;
-    registerInstrumentations = require('@opentelemetry/instrumentation').registerInstrumentations;
-  } else {
-    W3CTraceContextPropagator = require("@opentelemetry/core").W3CTraceContextPropagator;
-  }
+  return async () => {
+    let getNodeAutoInstrumentations;
+    let registerInstrumentations;
+    let W3CTraceContextPropagator;
 
-  const options = {
-    tags: [],
-    host: process.env.TRACING_COLLECTOR_HOST,
-    port: process.env.TRACING_COLLECTOR_PORT,
-    maxPacketSize: 65000
-  }
+    // If this is the server, it's all autoinstrumented.
+    // Else include the propagator.
+    if (context === 'server') {
+      getNodeAutoInstrumentations = require('@opentelemetry/auto-instrumentations-node').getNodeAutoInstrumentations;
+      registerInstrumentations = require('@opentelemetry/instrumentation').registerInstrumentations;
+    } else {
+      W3CTraceContextPropagator = require("@opentelemetry/core").W3CTraceContextPropagator;
+    }
 
-  const resources = new Resource({
-    [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
-  });
+    const options = {
+      tags: [],
+      host: process.env.TRACING_COLLECTOR_HOST,
+      port: process.env.TRACING_COLLECTOR_PORT,
+      maxPacketSize: 65000
+    }
 
-  // Create a tracer provider
-  const provider = new NodeTracerProvider({
-    resource: resources,
-  });
+    const detected = await envDetector.detect();
+    console.log(detected);
 
-  // Export to Jaeger
-  //const exporter = new JaegerExporter(options);
-  const exporter = new OTLPTraceExporter({
-    url: 'http://agent:4317'
-  });
+    const resources = new Resource({
+      [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
+    }).merge(detected);
 
-  // Use simple span (should probably use Batch)
-  const processor = new SimpleSpanProcessor(exporter);
-  provider.addSpanProcessor(processor);
-  provider.register();
-
-
-  // Create a new header for propagation from a given span
-  let createPropagationHeader;
-  if (context === 'requester') {
-    const propagator = new W3CTraceContextPropagator();
-    createPropagationHeader = (span) => {
-      let carrier = {};
-      // Inject the current trace context into the carrier object
-      propagator.inject(
-          api.trace.setSpanContext(api.ROOT_CONTEXT, span.spanContext()),
-          carrier,
-          api.defaultTextMapSetter
-      );
-      return carrier;
-    };
-  }
-
-  // Enable everything that's available for auto-instrumentation
-  // if this is the server.
-  if (context === 'server') {
-    registerInstrumentations({
-      instrumentations: [getNodeAutoInstrumentations()],
+    // Create a tracer provider
+    const provider = new NodeTracerProvider({
+      resource: resources,
     });
-  }
 
-  // Return instances of the API and the tracer to the calling app
-  return {
-    tracer: api.trace.getTracer(serviceName),
-    api: api,
-    propagator: (context === 'requester') ? createPropagationHeader : undefined,
-  }
+    // Export to Jaeger
+    //const exporter = new JaegerExporter(options);
+    const exporter = new OTLPTraceExporter({
+      url: 'http://agent:4317'
+    });
+
+    // Use simple span (should probably use Batch)
+    const processor = new SimpleSpanProcessor(exporter);
+    provider.addSpanProcessor(processor);
+    provider.register();
+
+
+    // Create a new header for propagation from a given span
+    let createPropagationHeader;
+    if (context === 'requester') {
+      const propagator = new W3CTraceContextPropagator();
+      createPropagationHeader = (span) => {
+        let carrier = {};
+        // Inject the current trace context into the carrier object
+        propagator.inject(
+            api.trace.setSpanContext(api.ROOT_CONTEXT, span.spanContext()),
+            carrier,
+            api.defaultTextMapSetter
+        );
+        return carrier;
+      };
+    }
+
+    // Enable everything that's available for auto-instrumentation
+    // if this is the server.
+    if (context === 'server') {
+      registerInstrumentations({
+        instrumentations: [getNodeAutoInstrumentations()],
+      });
+    }
+
+    // Return instances of the API and the tracer to the calling app
+    return {
+      tracer: api.trace.getTracer(serviceName),
+      api: api,
+      propagator: (context === 'requester') ? createPropagationHeader : undefined,
+    }
+  };
 };
